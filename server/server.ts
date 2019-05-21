16fbf2ba3810109f219e28f8545cfb4b9c4ae68f
Thalles Torma <thallestorma@gmail.com>
2019-05-21 14:04:22 -0300

    PATCH e DELETE finalizados

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,7 @@ import * as restify from 'restify'
 import * as mongoose from 'mongoose'
 import { environment } from '../common/environment'
 import { Router } from '../common/router'
+import { mergePatchBodyParser } from './merge-patch.parser'
 
 export class Server {
 
@@ -24,6 +25,7 @@ export class Server {
 
                 this.application.use(restify.plugins.queryParser())
                 this.application.use(restify.plugins.bodyParser())
+                this.application.use(mergePatchBodyParser)
 
                 for(let router of routers) {
                     router.applyRoutes(this.application)
--- users/users.router.ts
+++ users/users.router.ts
@@ -45,6 +45,29 @@ class UsersRouter extends Router {
                 return next()
             })
         })
+
+        application.patch('/users/:id', (req, resp, next) => {
+            const options = { new : true }
+            User.findByIdAndUpdate(req.params.id, req.body, options).then(user => {
+                if(user) {
+                    resp.json(user)
+                    return next()
+                }
+                resp.send(404)
+                return next()
+            })
+        })
+
+        application.del('/users/:id', (req, resp, next) => {
+            User.remove({ _id: req.params.id }).exec().then((cmdResult: any) => {
+                if(cmdResult.result.n) {
+                    resp.send(204)
+                } else {
+                    resp.send(404)
+                }
+                return next()
+            })
+        })
     }
 }
 
