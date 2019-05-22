0962f04a5617f322732410bc24d9f073a5d5be98
Thalles Torma <thallestorma@gmail.com>
2019-05-21 23:05:01 -0300

    Adicionado Middlewares para criptografar o password com bcrypt

--- users/users.model.ts
+++ users/users.model.ts
@@ -1,5 +1,7 @@
 import * as mongoose from 'mongoose'
+import * as bcrypt from 'bcrypt'
 import { validateCPF } from '../common/validators'
+import { environment } from '../common/environment'
 
 export interface User extends mongoose.Document {
     name: string,
@@ -39,5 +41,34 @@ const userSchema = new mongoose.Schema({
         }
     }
 })
+
+const hashPassword = (obj, next) => {
+    bcrypt.hash(obj.password, environment.security.saltRounds).then(hash => {
+        obj.password = hash
+        next()
+    }).catch(next)
+}
+
+const saveMiddleware = function (next) {
+    const user: User = this
+
+    if (!user.isModified('password')) {
+        next()
+    } else {
+        hashPassword(user, next)
+    }
+}
+
+const updateMiddleware = function (next) {
+    if (!this.getUpdate().password) {
+        next()
+    } else {
+        hashPassword(this.getUpdate(), next)
+    }
+}
+
+userSchema.pre('save', saveMiddleware)
+userSchema.pre('findOneAndUpdate', updateMiddleware)
+userSchema.pre('update', updateMiddleware)
 
 export const User = mongoose.model<User>('User', userSchema)
--- users/users.router.ts
+++ users/users.router.ts
@@ -33,7 +33,7 @@ class UsersRouter extends Router {
         })
 
         application.put('/users/:id', (req, resp, next) => {
-            const options = { overwrite: true }
+            const options = { runValidators: true, overwrite: true }
             User.update({ _id: req.params.id }, req.body, options).exec().then(result => {
                 if(result.n) {
                     return User.findById(req.params.id)
@@ -45,7 +45,7 @@ class UsersRouter extends Router {
         })
 
         application.patch('/users/:id', (req, resp, next) => {
-            const options = { new : true }
+            const options = { runValidators: true, new : true }
             User.findByIdAndUpdate(req.params.id, req.body, options)
                 .then(this.render(resp, next))
                 .catch(next)
