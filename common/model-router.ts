e9ff0d35beab1f0bcaf512dbf3693d2857cb3fca
Thalles Torma <thallestorma@gmail.com>
2019-06-04 18:28:37 -0300

    Refatoracao do Router(criado ModelRouter) e desenvolvimento do Restaurant e do Review

---
+++ common/model-router.ts
@@ -0,0 +1,66 @@
+import * as mongoose from 'mongoose'
+import { Router } from './router'
+import { NotFoundError } from 'restify-errors'
+
+export abstract class ModelRouter<D extends mongoose.Document> extends Router {
+    constructor(protected model: mongoose.Model<D>) {
+        super()
+    }
+
+    validateId = (req, resp, next) => {
+        if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+            next(new NotFoundError('Document not found'))
+        } else {
+            next()
+        }
+    }
+
+    findAll = (req, resp, next) => {
+        this.model.find()
+            .then(this.renderAll(resp, next))
+            .catch(next)
+    }
+
+    findById = (req, resp, next) => {
+        this.model.findById(req.params.id)
+            .then(this.render(resp, next))
+            .catch(next)
+    }
+
+    save = (req, resp, next) => {
+        let document = new this.model(req.body)
+        document.save()
+            .then(this.render(resp, next))
+            .catch(next)
+    }
+
+    replace = (req, resp, next) => {
+        const options = { runValidators: true, overwrite: true }
+        this.model.update({ _id: req.params.id }, req.body, options).exec().then(result => {
+            if(result.n) {
+                return this.model.findById(req.params.id)
+            } else {
+                throw new NotFoundError('Documento não encontrado')
+            }
+        }).then(this.render(resp, next))
+          .catch(next)
+    }
+
+    update = (req, resp, next) => {
+        const options = { runValidators: true, new : true }
+        this.model.findByIdAndUpdate(req.params.id, req.body, options)
+            .then(this.render(resp, next))
+            .catch(next)
+    }
+
+    delete = (req, resp, next) => {
+        this.model.remove({ _id: req.params.id }).exec().then((cmdResult: any) => {
+            if(cmdResult.result.n) {
+                resp.send(204)
+            } else {
+                throw new NotFoundError('Documento não encontrado')
+            }
+            return next()
+        }).catch(next)
+    }
+}
--- users/users.router.ts
+++ users/users.router.ts
@@ -1,66 +1,23 @@
 import * as restify from 'restify'
-import { Router } from '../common/router'
-import { NotFoundError } from 'restify-errors'
+import { ModelRouter } from '../common/model-router'
 import { User } from './users.model'
 
-class UsersRouter extends Router {
+class UsersRouter extends ModelRouter<User> {
 
     constructor() {
-        super()
+        super(User)
         this.on('beforeRender', document => {
             document.password = undefined
         })
     }
 
     applyRoutes(application: restify.Server) {
-        application.get('/users', (req, resp, next) => {
-            User.find()
-                .then(this.render(resp, next))
-                .catch(next)
-        })
-
-        application.get('/users/:id', (req, resp, next) => {
-            User.findById(req.params.id)
-                .then(this.render(resp, next))
-                .catch(next)
-        })
-
-        application.post('/users', (req, resp, next) => {
-            let user = new User(req.body)
-            user.save()
-                .then(this.render(resp, next))
-                .catch(next)
-        })
-
-        application.put('/users/:id', (req, resp, next) => {
-            const options = { runValidators: true, overwrite: true }
-            User.update({ _id: req.params.id }, req.body, options).exec().then(result => {
-                if(result.n) {
-                    return User.findById(req.params.id)
-                } else {
-                    throw new NotFoundError('Documento não encontrado')
-                }
-            }).then(this.render(resp, next))
-              .catch(next)
-        })
-
-        application.patch('/users/:id', (req, resp, next) => {
-            const options = { runValidators: true, new : true }
-            User.findByIdAndUpdate(req.params.id, req.body, options)
-                .then(this.render(resp, next))
-                .catch(next)
-        })
-
-        application.del('/users/:id', (req, resp, next) => {
-            User.remove({ _id: req.params.id }).exec().then((cmdResult: any) => {
-                if(cmdResult.result.n) {
-                    resp.send(204)
-                } else {
-                    throw new NotFoundError('Documento não encontrado')
-                }
-                return next()
-            }).catch(next)
-        })
+        application.get('/users', this.findAll)
+        application.get('/users/:id', [this.validateId, this.findById])
+        application.post('/users', this.save)
+        application.put('/users/:id', [this.validateId, this.replace])
+        application.patch('/users/:id', [this.validateId, this.update])
+        application.del('/users/:id', [this.validateId, this.delete])
     }
 }
 
