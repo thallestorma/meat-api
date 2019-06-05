42b1781c1f02e9b0e47d9e5ddf5a21ec09d58a35
Thalles Torma <thallestorma@gmail.com>
2019-06-05 18:30:58 -0300

    Adicionando links na API

--- reviews/reviews.router.ts
+++ reviews/reviews.router.ts
@@ -14,9 +14,16 @@ class ReviewsRouter extends ModelRouter<Review> {
     }
 
     applyRoutes(application: restify.Server) {
-        application.get('/reviews', this.findAll)
-        application.get('/reviews/:id', [this.validateId, this.findById])
-        application.post('/reviews', this.save)
+        application.get(`${this.basePath}`, this.findAll)
+        application.get(`${this.basePath}/:id`, [this.validateId, this.findById])
+        application.post(`${this.basePath}`, this.save)
+    }
+
+    envelope(document) {
+        let resource = super.envelope(document)
+        const restId = document.restaurant._id ? document.restaurant._id : document.restaurant
+        resource._links.restaurant = `/restaurants/${restId}`
+        return resource
     }
 }
 
--- users/users.router.ts
+++ users/users.router.ts
@@ -23,13 +23,13 @@ class UsersRouter extends ModelRouter<User> {
     }
 
     applyRoutes(application: restify.Server) {
-        application.get({path: '/users', version: '2.0.0'}, [this.findByEmail, this.findAll])
-        application.get({path: '/users', version: '1.0.0'}, this.findAll)
-        application.get('/users/:id', [this.validateId, this.findById])
-        application.post('/users', this.save)
-        application.put('/users/:id', [this.validateId, this.replace])
-        application.patch('/users/:id', [this.validateId, this.update])
-        application.del('/users/:id', [this.validateId, this.delete])
+        application.get({path: `${this.basePath}`, version: '2.0.0'}, [this.findByEmail, this.findAll])
+        application.get({path: `${this.basePath}`, version: '1.0.0'}, this.findAll)
+        application.get(`${this.basePath}/:id`, [this.validateId, this.findById])
+        application.post(`${this.basePath}`, this.save)
+        application.put(`${this.basePath}/:id`, [this.validateId, this.replace])
+        application.patch(`${this.basePath}/:id`, [this.validateId, this.update])
+        application.del(`${this.basePath}/:id`, [this.validateId, this.delete])
     }
 }
 
